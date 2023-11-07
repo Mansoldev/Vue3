1a41401625101a837e819ef9f95769ccd1d9e845
Mario García Alarcón <mariogarciaalarcon@gmail.com>
2023-11-07 16:33:56 +0100

    placehold data

--- src/pokemons/composables/usePokemons.ts
+++ src/pokemons/composables/usePokemons.ts
@@ -2,12 +2,14 @@ import { computed, watchEffect } from 'vue'
 import { useQuery } from '@tanstack/vue-query'
 
 import { getPokemons } from '../helpers/get-pokemons'
+import { initialPokemons } from '../data/initial-pokemons'
 
 export const usePokemons = () => {
     const { isLoading, data:pokemons, isError, error } = useQuery({
         queryKey: ['pokemons'],
         queryFn: getPokemons,
-        // retry: 1,
+        placeholderData: initialPokemons,
+        //retry: 1,
         // retryDelay: 5000
     })
 
