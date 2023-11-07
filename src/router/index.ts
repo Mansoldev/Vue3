8b37a1d4040c8cd68bb3e86a68775ac124060c6b
Mario García Alarcón <mariogarciaalarcon@gmail.com>
2023-11-07 10:36:16 +0100

    Navbar + style menu

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../shared/views/HomeView.vue'
+import HomeView from '@/shared/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,10 +12,15 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../shared/views/AboutView.vue')
+      component: () => import('@/shared/views/AboutView.vue')
+    },
+    //Rutas que no existen > Te lleva al home
+    {
+      path: '/:pathmatch(.*)*',
+      redirect: () => { 
+        console.log('La ruta no existe')
+        return { name: 'home' }
+      },
     }
   ]
 })
---
+++ src/shared/components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav>
+        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="25" height="25" />
+        <span class="appTitle">CompoApp</span>
+
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+    </nav>
+</template>
+
+<style>
+nav {
+  align-items: flex-start;
+  display: flex;
+  font-size: 12px;
+  margin-top: 10px;
+  text-align: center;
+  width: 100%;
+}
+
+.logo {
+    margin-right: 5px;
+}
+.appTitle {
+    margin-right: 10px;
+}
+a {
+    margin-right: 5px;
+}
+
+nav a {
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
+}
+nav a:first-of-type {
+    border: 0;
+}
+nav a.router-link-exact-active {
+    color: var(--color-text);
+}
+nav a.router-link-exact-active:hover {
+    background-color: transparent;
+}
+</style>
