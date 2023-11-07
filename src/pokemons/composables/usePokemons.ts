b717a4488f23f2f988d85df6fe18b2e324741268
Mario García Alarcón <mariogarciaalarcon@gmail.com>
2023-11-07 13:06:39 +0100

    composable usePokemon with global state

--- src/pokemons/composables/usePokemons.ts
+++ src/pokemons/composables/usePokemons.ts
@@ -1,16 +1,27 @@
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import { getPokemons } from '../helpers/get-pokemons'
 import type { Pokemon } from '../interfaces'
 
-export const usePokemons = () => {
-    const pokemons = ref<Pokemon[]>([])
-    const isLoading = ref(true)
+const pokemons = ref<Pokemon[]>([])
+const isLoading = ref(true)
 
-    getPokemons()
-        .then( data => {
-            pokemons.value = data
-            isLoading.value = false
-        })
+export const usePokemons = () => {
+    // if (pokemons.value.length === 0) {
+    //     getPokemons()
+    //         .then( data => {
+    //             pokemons.value = data
+    //             isLoading.value = false
+    //         })
+    // }
+    
+    const loadPokemons = async() => {
+        const data = await getPokemons()
+        pokemons.value = data
+        isLoading.value = false
+    }
+    onMounted(async() => {
+        await loadPokemons()
+    })
 
     return {
         pokemons,
