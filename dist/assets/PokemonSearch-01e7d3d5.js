279b2117fe280b638265879453c9d9c23ca133a9
Mario García Alarcón <mariogarciaalarcon@gmail.com>
2023-11-09 16:14:25 +0100

    dist

---
+++ dist/assets/PokemonSearch-01e7d3d5.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c,a as n}from"./index-40a515b9.js";const s={},t=n("h2",null,"pokemon search",-1),a=[t];function r(_,l){return o(),c("div",null,a)}const f=e(s,[["render",r]]);export{f as default};
