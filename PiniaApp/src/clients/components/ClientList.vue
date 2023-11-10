b9b38b1adfaf6da776549888b8b49ebd8cf462d4
Mario García Alarcón <mariogarciaalarcon@gmail.com>
2023-11-10 13:55:40 +0100

    Client list first steps

---
+++ PiniaApp/src/clients/components/ClientList.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <ul>
+        <li>
+            <RouterLink :to="{
+                name: 'client-id', 
+                params: {id: 1}
+            }">
+                ClientName
+            </RouterLink>"
+        </li>
+    </ul>
+</template>
--- PiniaApp/src/clients/pages/ListPage.vue
+++ PiniaApp/src/clients/pages/ListPage.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-
+import ClientList from '../components/ClientList.vue';
 </script>
 
 <template>
     <div>
         <h1>Clients List</h1>
+
+        <ClientList/>
+
+        <!-- Pagination Numbers -->
+
+        <!-- LoadingModal -->
     </div>
 </template>
